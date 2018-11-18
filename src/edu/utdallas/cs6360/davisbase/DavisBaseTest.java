9c5c7a57ef9988ca8ac3c6029726e6b666de9574
Cory Krol <coryteee@gmail.com>
2018-11-18 01:04:13 -0600

    -First revision of DataRecord class complete. -Test class was created and one test to test converting from DataRecord to bytes and back -Restructured files for packages and restructured some of the Davisbase.java code at the suggestion of my IDE

---
+++ src/edu/utdallas/cs6360/davisbase/DavisBaseTest.java
@@ -0,0 +1,33 @@
+package edu.utdallas.cs6360.davisbase;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * JUnit Test class for Davisbase
+ */
+class DavisBaseTest {
+	
+	@BeforeEach
+	void setUp() {
+	}
+	
+	@AfterEach
+	void tearDown() {
+	}
+	
+	@Test
+	void main() {
+	}
+	
+	@Test
+	void printCmd() {
+	}
+	
+	@Test
+	void printDef() {
+	}
+}
